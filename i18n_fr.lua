a74da1c3909343eef21e9fcff791c9f2f5854474
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:37 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,3 @@
+return {
+    ["Draw letters and find the longest word; draw numbers and hit the target."] = { fr = "Tirez des lettres et trouvez le mot le plus long ; tirez des nombres et atteignez la cible." },
+}
