fe6caebd51822834563c1648d5c7090e44b4d983
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:25:19 +0200

    chore: bump version to 1.1.5

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "chiffreslettres",
     fullname    = _("Chiffres et Lettres"),
     description = _("Draw letters and find the longest word; draw numbers and hit the target."),
-    version     = "1.1.4",
+    version     = "1.1.5",
 }
